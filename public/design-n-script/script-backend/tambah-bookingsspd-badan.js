397e7e723f5bc14e5f646c27eb0b1089f4bbc5c2
unknown <farrassyauqi22803@gmail.com>
2025-10-03 22:40:37 +0700

    Update V3

--- public/design-n-script/script-backend/tambah-bookingsspd-badan.js
+++ public/design-n-script/script-backend/tambah-bookingsspd-badan.js
@@ -75,6 +75,7 @@ document.getElementById('formBadanUsaha_Bphtb').addEventListener('submit', async
     const formattedTanggal = `${day.padStart(2, '0')}-${month.padStart(2, '0')}-${year}`;
     console.log('Tanggal yang akan dikirim:', formattedTanggal);
     //
+    //kolom tabel pat_1_bookingsspd (noppbb)
     const digitprovinsi = document.getElementById('digitprovinsi').value;  // 2 digit provinsi
     const digitkabupatenkota = document.getElementById('digitkabupatenkota').value;  // 2 digit kabupaten/kota
     const digitkecamatan = document.getElementById('digitkecamatan').value;  // 3 digit kecamatan
@@ -82,16 +83,17 @@ document.getElementById('formBadanUsaha_Bphtb').addEventListener('submit', async
     const digitblok = document.getElementById('digitblok').value;  // 3 digit Blok
     const digiturut = document.getElementById('digiturut').value;  // 4 digit Urut
     const digitpajak = document.getElementById('digitpajak').value;  // 1 digit pajak
-
     // Gabungkan menjadi NOP PBB
     const noppbb = `${digitprovinsi.padStart(2, '0')}.${digitkabupatenkota.padStart(2, '0')}.${digitkecamatan.padStart(3, '0')}.${digitkpp.padStart(3, '0')}.${digitblok.padStart(3, '0')}.${digiturut.padStart(4, '0')}.${digitpajak.padStart(1,'0')}`;
 
+    //kolom tabel pat_1_bookingsspd (namawajibpajak, alamatwajibpajak, namapemilikobjekpajak, alamatpemilikobjekpajak)
     // Ambil data lainnya untuk booking
     const namawajibpajak = document.getElementById('namawajibpajak').value;
     const alamatwajibpajak = document.getElementById('alamatwajibpajak').value;
     const namapemilikobjekpajak = document.getElementById('namapemilikobjekpajak').value;
     const alamatpemilikobjekpajak = document.getElementById('alamatpemilikobjekpajak').value;
     //npwp WP
+    //kolom tabel pat_1_bookingsspd (npwpwp)
     const spesial_1wp = document.getElementById('unik1wp').value;
     const spesial_2wp = document.getElementById('unik2wp').value;
     const spesial_3wp = document.getElementById('unik3wp').value;
@@ -101,6 +103,7 @@ document.getElementById('formBadanUsaha_Bphtb').addEventListener('submit', async
     const npwpwp = `${spesial_1wp.padStart(2, '0')}.${spesial_2wp.padStart(3, '0')}.${spesial_3wp.padStart(3, '0')}.${spesial_4wp}-${kppwp.padStart(3, '0')}.${pusatcwp.padStart(3, '0')}`;
 
     //npwp OP
+    //kolom tabel pat_1_bookingsspd (npwpop)
     const spesial_1op = document.getElementById('unik1op').value;
     const spesial_2op = document.getElementById('unik2op').value;
     const spesial_3op = document.getElementById('unik3op').value;
@@ -110,20 +113,25 @@ document.getElementById('formBadanUsaha_Bphtb').addEventListener('submit', async
     const npwpop = `${spesial_1op.padStart(2, '0')}.${spesial_2op.padStart(3, '0')}.${spesial_3op.padStart(3, '0')}.${spesial_4op}-${kppop.padStart(3, '0')}.${pusatcop.padStart(3, '0')}`;
 
     //penghitungan otomatis pada bagian njop
+    //kolom tabel pat_5_penghitungan_njop (luas_tanah, njop_tanah, luas_bangunan, njop_bangunan)
     const luas_tanah = document.getElementById('luas_tanah').value;
     const njop_tanah = document.getElementById('njop_tanah').value;
     const luas_bangunan = document.getElementById('luas_bangunan').value;
     const njop_bangunan = document.getElementById('njop_bangunan').value;
     // Ambil data BPHTB
+    //kolom tabel pat_2_bphtb_perhitungan (bphtb_yangtelah_dibayar)
     const bphtb_yangtelah_dibayar = document.getElementById('bphtb_yangtelah_dibayar').value;
+    //kolom tabel pat_4_objek_pajak (harga_transaksi, letaktanahdanbangunan)
     const hargatransaksi = document.getElementById('hargatransaksi').value;
     const letaktanahdanbangunan = document.getElementById('letaktanahdanbangunan').value;
 
+    //kolom tabel pat_4_objek_pajak (jenis_perolehan)
      const jenisPerolehan = document.getElementById('jenisperolehan').value;
+     //kolom tabel pat_2_bphtb_perhitungan (npoptkpMap/nilaiPerolehanObjekPajakTidakKenaPajak)
     const npoptkpMap = {
         '03': 300000000,
-        '04': 300000000,
-        '05': 300000000,
+        '04': 400000000,
+        '05': 400000000,
         '24': 300000000,
         '30': 300000000,
         '28': 40000000,
@@ -131,18 +139,21 @@ document.getElementById('formBadanUsaha_Bphtb').addEventListener('submit', async
         '34': 0
     };
     const nilaiPerolehanObjekPajakTidakKenaPajak = npoptkpMap[jenisPerolehan] ?? 80000000;
-
+    //kolom tabel pat_4_objek_pajak (rt_rwobjekpajak, kelurahandesalp, kecamatanlp)
     const rt_rwobjekpajak = document.getElementById('rt_rwobjekpajak').value;
     const kelurahandesalp = document.getElementById('kelurahandesalp').value;
     const kecamatanlp = document.getElementById('kecamatanlp').value;
     // Ambil nilai dari dropdown status kepemilikan, jika tidak ada pilihannya, set default 'Milik Pribadi'
+    //kolom tabel pat_4_objek_pajak (status_kepemilikan)
     const status_kepemilikan = document.getElementById('status_kepemilikan').value || 'Milik Pribadi';
+    //kolom tabel pat_4_objek_pajak (keterangan)
     const keterangan = document.getElementById('keterangan').value;
+    //kolom tabel pat_4_objek_pajak (nomor_sertifikat)
     const nomor_sertifikat = document.getElementById('nomor_sertifikat').value;
-
+    //kolom tabel pat_4_objek_pajak (tanggal_perolehan, tanggal_pembayaran, nomor_bukti_pembayaran)
     const formattedPerolehan = `${document.getElementById('tanggaloleh').value.padStart(2, '0')}-${document.getElementById('bulanoleh').value.padStart(2, '0')}-${document.getElementById('tahunoleh').value}`;
     const formattedPembayaran = `${document.getElementById('tanggalbayar').value.padStart(2, '0')}-${document.getElementById('bulanbayar').value.padStart(2, '0')}-${document.getElementById('tahunbayar').value}`;
-
+    //kolom tabel pat_4_objek_pajak (nomor_bukti_pembayaran)
     const nomor_bukti_pembayaran = document.getElementById('nomor_bukti_pembayaran').value;
 
     // Mengambil userID dari sessionStorage atau localStorage
@@ -154,6 +165,7 @@ document.getElementById('formBadanUsaha_Bphtb').addEventListener('submit', async
     }
     // Data yang akan dikirim ke backend
     const data = {
+        // Data booking utama (pat_1_bookingsspd)
         userid,  // ID pengguna yang sedang login (bisa diambil dari session)
         jenis_wajib_pajak: 'Badan Usaha', // Jenis wajib pajak yang dipilih (badan usaha)
         nobooking,
@@ -163,45 +175,45 @@ document.getElementById('formBadanUsaha_Bphtb').addEventListener('submit', async
         namapemilikobjekpajak,
         alamatpemilikobjekpajak,
         tanggal: formattedDate,
-        tahunajb: document.getElementById('tahunajb').value,
-        kabupatenkotawp: document.getElementById('kabupatenkotawp').value,
-        kecamatanwp: document.getElementById('kecamatanwp').value,
-        kelurahandesawp: document.getElementById('kelurahandesawp').value,
-        rtrwwp: document.getElementById('rtrwwp').value,
-        npwpwp: npwpwp,
-        kodeposwp: document.getElementById('kodeposwp').value,
-        kabupatenkotaop: document.getElementById('kabupatenkotaop').value,
-        kecamatanop: document.getElementById('kecamatanop').value,
-        kelurahandesaop: document.getElementById('kelurahandesaop').value,
-        rtrwop: document.getElementById('rtrwop').value,
-        npwpop: npwpop,
-        kodeposop: document.getElementById('kodeposop').value,
+        tahunajb: document.getElementById('tahunajb').value, //kolom tabel pat_1_bookingsspd (tahunajb)
+        kabupatenkotawp: document.getElementById('kabupatenkotawp').value, //kolom tabel pat_1_bookingsspd (kabupatenkotawp)
+        kecamatanwp: document.getElementById('kecamatanwp').value, //kolom tabel pat_1_bookingsspd (kecamatanwp)
+        kelurahandesawp: document.getElementById('kelurahandesawp').value, //kolom tabel pat_1_bookingsspd (kelurahandesawp)
+        rtrwwp: document.getElementById('rtrwwp').value, //kolom tabel pat_1_bookingsspd (rtrwwp)
+        npwpwp: npwpwp, //kolom tabel pat_1_bookingsspd (npwpwp)
+        kodeposwp: document.getElementById('kodeposwp').value, //kolom tabel pat_1_bookingsspd (kodeposwp)
+        kabupatenkotaop: document.getElementById('kabupatenkotaop').value, //kolom tabel pat_1_bookingsspd (kabupatenkotaop)
+        kecamatanop: document.getElementById('kecamatanop').value, //kolom tabel pat_1_bookingsspd (kecamatanop)
+        kelurahandesaop: document.getElementById('kelurahandesaop').value, //kolom tabel pat_1_bookingsspd (kelurahandesaop)
+        rtrwop: document.getElementById('rtrwop').value, //kolom tabel pat_1_bookingsspd (rtrwop)
+        npwpop: npwpop, //kolom tabel pat_1_bookingsspd (npwpop)
+        kodeposop: document.getElementById('kodeposop').value, //kolom tabel pat_1_bookingsspd (kodeposop)
 
-        // Penghitungan luas x njop
-        luas_tanah,
-        njop_tanah,
-        luas_bangunan,
-        njop_bangunan,
+        // Penghitungan luas x njop (pat_5_penghitungan_njop)
+        luas_tanah, //kolom tabel pat_5_penghitungan_njop (luas_tanah)
+        njop_tanah, //kolom tabel pat_5_penghitungan_njop (njop_tanah)
+        luas_bangunan, //kolom tabel pat_5_penghitungan_njop (luas_bangunan)
+        njop_bangunan, //kolom tabel pat_5_penghitungan_njop (njop_bangunan)
 
-        // Data perhitungan BPHTB
-        nilaiPerolehanObjekPajakTidakKenaPajak,
-        bphtb_yangtelah_dibayar,
+        // Data perhitungan BPHTB (pat_2_bphtb_perhitungan)
+        nilaiPerolehanObjekPajakTidakKenaPajak, //kolom tabel pat_2_bphtb_perhitungan (nilaiPerolehanObjekPajakTidakKenaPajak)
+        bphtb_yangtelah_dibayar, //kolom tabel pat_2_bphtb_perhitungan (bphtb_yangtelah_dibayar)
 
-        // Data Objek Pajak
-        hargatransaksi,
+        // Data Objek Pajak (pat_4_objek_pajak)
+        hargatransaksi, //kolom tabel pat_4_objek_pajak (hargatransaksi)
         letaktanahdanbangunan, 
-        rt_rwobjekpajak,
+        rt_rwobjekpajak, //kolom tabel pat_4_objek_pajak (rt_rwobjekpajak)
         kecamatanlp,
-        kelurahandesalp, 
+        kelurahandesalp, //kolom tabel pat_4_objek_pajak (kelurahandesalp)
         status_kepemilikan,
-        jenisPerolehan, 
-        keterangan, 
+        jenisPerolehan, //kolom tabel pat_4_objek_pajak (jenisPerolehan)
+        keterangan, //kolom tabel pat_4_objek_pajak (keterangan)
         nomor_sertifikat, 
-        tanggal_perolehan: formattedPerolehan, 
-        tanggal_pembayaran: formattedPembayaran, 
-        nomor_bukti_pembayaran,
+        tanggal_perolehan: formattedPerolehan, //kolom tabel pat_4_objek_pajak (tanggal_perolehan)
+        tanggal_pembayaran: formattedPembayaran, //kolom tabel pat_4_objek_pajak (tanggal_pembayaran)
+        nomor_bukti_pembayaran, //kolom tabel pat_4_objek_pajak (nomor_bukti_pembayaran)
 
-        trackstatus: 'Draft'
+        trackstatus: 'Draft', //kolom tabel pat_1_bookingsspd (trackstatus)
     };
 
     // Kirim data booking dan perhitungan BPHTB ke backend menggunakan fetch dan async/await
